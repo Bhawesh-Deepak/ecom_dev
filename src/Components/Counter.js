8a58deb9af73d18a2ea1e46d208ce51294c629c3
Bhawesh Deepak <bhawesh.deepak@basserah.com>
2023-12-19 03:14:20 -0800

    Add update value for initial set values

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -2,12 +2,17 @@ import React,{useState} from 'react'
 
 function Counter() {
   const [counter, setCounter]= useState(0)  
-    
+  const [initialValue, setInitialValue]= useState(0);
+
+  const updateValue=()=>{
+    setCounter(initialValue);
+  }
   return (
     <div>
         <p>Current Counter value : {counter}</p>
          <button onClick={()=> setCounter(counter+1)}>Increment</button>  
          <button onClick={()=> setCounter(counter-1)}>Decrement</button>   
+         <input type='text' value={initialValue} onChange={(e)=>setInitialValue(parseInt(e.target.value) || 0)} /> <button onClick={updateValue}>Update Value</button>
     </div>
   )
 }
